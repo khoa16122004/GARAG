4cf0924f8eff43e5b208d1ec68b3fa147a8c876c
khoa16122004 <khoatr.work@gmail.com>
2025-05-21 15:09:15 +0700

    Update retriever.py

--- src/reader.py
+++ src/reader.py
@@ -67,6 +67,9 @@ class Reader(torch.nn.Module):
     def _cal_label_prob(self, probs, labels):
         result = []
         for prob, label in zip(probs, labels):
+            print("Prob: ", prob)
+            print("Label: ", label)
+            input("Press Enter to continue...")
             mask = label > 0
             prob, label = prob[mask], label[mask]
             log_softmax = torch.nn.functional.log_softmax(prob, dim=-1)
